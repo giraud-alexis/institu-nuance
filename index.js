6cef64afa4736d6d1ce1a159bd6cff571d693982
giraud-alexis <140321581+giraud-alexis@users.noreply.github.com>
2023-08-16 19:15:25 +0200

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-50px";
+  }
+  lastScroll = window.scrollY;
+});
